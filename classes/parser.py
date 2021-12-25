8c7d3e4f8a97c736bc8fd4fbaa51b475bdb95d2d
Isaac Lyons <isaac@snowskeleton.net>
2021-12-25 15:31:11 -0500

    Moved parsing of arguments out to its own file to clean up the main file.

---
+++ classes/parser.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+
+def parseThis(thing):
+    parser = reqparse.RequestParser()
+    parser.add_argument('serialNumber', required=False, type=str)
+    parser.add_argument('modelNumber', required=False, type=str)
+    parser.add_argument('assetTag', required=False, type=str)
+    parser.add_argument('customer', required=False, type=str)
+    args = parser.parse_args()
+    print(args)
+    return args
