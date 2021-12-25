041af4b8d2431651c99dad94aee19705f171fde6
Isaac Lyons <isaac@snowskeleton.net>
2021-12-25 15:32:13 -0500

    Imporoved clarity.

--- firstRun.py
+++ firstRun.py
@@ -4,6 +4,6 @@ from pymongo import MongoClient
 cluster = MongoClient('mongodb+srv://snow:SH0zdIAD9scRU1if@cluster0.7luyp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 db = cluster["FixMe"]
 collection = db['nextNumber']
-post = {"nextNumber": 0}
+post = {"_id": "sequence", "nextNumber": 0}
 collection.insert_one(post)
 
