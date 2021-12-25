238bae0562baa6ee6a374c9d7599c04fb0d488e6
Isaac Lyons <isaac@snowskeleton.net>
2021-12-25 13:38:11 -0500

    Added setup script for database.

---
+++ firstRun.py
@@ -0,0 +1,9 @@
+from pprint import pprint
+from pymongo import MongoClient
+
+cluster = MongoClient('mongodb+srv://snow:SH0zdIAD9scRU1if@cluster0.7luyp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+db = cluster["FixMe"]
+collection = db['nextNumber']
+post = {"nextNumber": 0}
+collection.insert_one(post)
+
