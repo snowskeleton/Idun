46fea015ce37040e234e3960be90d1b55ff875ac
Isaac Lyons <isaac@snowskeleton.net>
2021-12-29 09:58:24 -0500

    Removed unused templates.

--- main.py
+++ main.py
@@ -50,47 +50,6 @@ class AddNoteToTicket(Resource):
         parser.add_argument("Note", required=True, type=str)
 
 
-class SetCompletedOverride(Resource):
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("TicketNumber", required=True, type=str)
-        # TODO set CompletedOverride value for given ticket number
-
-
-class BulkTickets(Resource):
-    def get(self):
-        # get all tickets, with the arguments specifying if whether to return Open tickets, Closed tickets, or both.
-        parser = reqparse.RequestParser()
-        parser.add_argument('Open', required=False, type=bool)
-        parser.add_argument('Closed', required=False, type=bool)
-
-        # allows the user to specify a field by which they want to filter. Not sure how.
-        parser.add_argument('Field', required=False, type=str)
-        # a value for the above.
-        parser.add_argument('Something', required=False, type=str)
-
-        # return requested tickets.
-
-
-class ListPartsFor(Resource):
-    def get(self):
-        parser = reqparse.RequestParser()
-        # list all parts compatible with specified model, as well as quantity surplus/deficit
-        parser.add_argument('Model', required=True, type=str)
-
-
-class AddUser(Resource):
-    def post(self):  # something something, make new user. TODO: make secure
-        parser = reqparse.RequestParser()
-        parser.add_argument('Name', required=True, type=str)
-        # set True for admin, False for non-admin. Password created elsewhere.
-        parser.add_argument('Admin', required=True, type=bool)
-
-        # with open("./data/customerlist.json", "r") as read_file:
-        #     data = json.loadk(read_file.to_dict())
-        #     print(data)
-
-
 
 api.add_resource(CreateNewTicket, '/api/createNewTicket')
 api.add_resource(AddPartsToTicket, '/api/addPartsToTicket')
